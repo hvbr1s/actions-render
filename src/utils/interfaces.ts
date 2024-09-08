a936f3c0956c3b591a96a5363bfbcb0ce3091ad2
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2024-09-08 12:01:25 +0200

    Update interfaces.ts

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,15 +1,21 @@
 export interface NFTConfig {
-    uploadPath: string;
-    imgFileName: string;
-    imgType: string;
-    imgName: string;
-    description: string;
-    attributes: Array<{
-      trait_type: string;
-      value: string;
+
+  uploadPath: string;
+  imgFileName: string;
+  imgType: string;
+
+  imgName: string;
+  description: string;
+  image: string;
+  attributes: Array<{
+    trait_type: string;
+    value: string;
+  }>;
+  properties: {
+    files: Array<{
+      uri: string;
+      type: string;
     }>;
-}
-  
-export interface UriConfig extends NFTConfig {
-    imageURI: string;
+    category: string;
+  };
 }
